350b6d0c5161b8a42b5c29def658c5924c41a1ad
prodigy3228 <davranmirsalimov57@gmail.com>
2026-05-26 01:53:42 +0600

    Register User model in admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,17 +5,3 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'phone_number']
     search_fields = ['user__username', 'user__email', 'phone_number']
-
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
-from .models import Profile
-
-# Стандартная регистрация пользователей
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
-
-# Регистрация профиля
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'phone_number']
