9858e17e356c1f0973b29009f8965f3495eb92fe
prodigy3228 <davranmirsalimov57@gmail.com>
2026-05-26 01:44:17 +0600

    Fix user admin registration

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,3 +5,16 @@ from .models import Profile
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'phone_number']
     search_fields = ['user__username', 'user__email', 'phone_number']
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+# Разрегистрируем стандартную модель, если она уже зарегистрирована
+if admin.site.is_registered(User):
+    admin.site.unregister(User)
+
+# Зарегистрируем заново со стандартным UserAdmin
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
